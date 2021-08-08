5422fadaa2eb81da7cecf47e00da716307b0c72d
Artvan15 <al_chechot@ukr.net>
2021-08-09 00:40:21 +0300

    full cover of Catch2 unit tests

--- Lippman.cpp
+++ Lippman.cpp
@@ -1,38 +1,7 @@
 ﻿#include <iostream>
-#include <stdexcept>
-#include <sstream>
-#include <exception>
-#include <functional>
 
 #include "Tests.h"
 
-using namespace std;
 
 
-void RunTest(void(*test_function)(), const char* test_name)
-{
-	try
-	{
-		test_function();
-		cout << "[+] " << test_name << " run successful\n";
-	}
-	catch(const exception& ex)
-	{
-		cout << "[-] " << test_name << " failed. " << ex.what() << endl;
-	}
-}
-
-
-
-
-
-int main()
-{
-	RunTest(TestSpeed, "TestSpeed");
-	RunTest(TestCollision, "TestCollision");
-	RunTest(TestCollisionLess, "TestCollisionLess");
-	RunTest(SpeedIsSaved, "SpeedIsSaved");
-	RunTest(AlertWhenImminent, "AlertWhenImminent");
-	
-}
 
--- Tests.h
+++ Tests.h
@@ -2,6 +2,8 @@
 
 #include "AutoBreak.h"
 #include <stdexcept>
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_approx.hpp"
 
 constexpr void CheckBoolExpression(bool expression, const char* msg)
 {
@@ -9,52 +11,63 @@ constexpr void CheckBoolExpression(bool expression, const char* msg)
 		throw std::runtime_error(msg);
 }
 
-void TestSpeed()
-{
-	MockServiceBus bus;
-	AutoBrake auto_brake(bus);
-	CheckBoolExpression(auto_brake.get_speed_mps() == 0, "speed isn't zero");
-}
+/* REQUIRE - Test ends immediately, if assert fail
+ * CHECK - Test continue to execute
+ * REQUIRE_THROWS - exception has to be thrown
+ * REQUIRE_THROWS_AS - second param is type of exception that has to be thrown
+ * REQUIRE_NOTHROW - no exception has to be thrown
+ * Catch::Approx - used to compare float values
+ * FAIL - leads to fail test
+ * SECTION - separate test in TEST_CASE. Insert former instructions into
+ *	every SECTION
+ */
 
-void TestCollision()
+TEST_CASE("AutoBrake")
 {
 	MockServiceBus bus;
 	AutoBrake auto_brake(bus);
-	CheckBoolExpression(auto_brake.get_collision_threshold_s() == 5, "collision isn't 5");
-}
 
-void TestCollisionLess()
-{
-	MockServiceBus bus;
-	AutoBrake auto_brake(bus);
-	auto_brake.set_collision_threshold_s(2);
-}
+	SECTION("initializes speed to zero")
+	{
+		REQUIRE(auto_brake.get_speed_mps() == Catch::Approx(0));
+	}
 
-void SpeedIsSaved()
-{
-	MockServiceBus bus;
-	AutoBrake auto_brake(bus);
+	SECTION("initializes sensitivity to five")
+	{
+		REQUIRE(auto_brake.get_collision_threshold_s() == Catch::Approx(5));
+	}
+
+	SECTION("throws when sensitivity less than one")
+	{
+		REQUIRE_THROWS(auto_brake.set_collision_threshold_s(0.5));
+	}
+
+	SECTION("saves speed after update")
+	{
+		bus.speed_update_callback(SpeedUpdate{ 100 });
+		REQUIRE(100 == auto_brake.get_speed_mps());
+		bus.speed_update_callback(SpeedUpdate{ 50 });
+		REQUIRE(50 == auto_brake.get_speed_mps());
+		bus.speed_update_callback(SpeedUpdate{ 0 });
+		REQUIRE(auto_brake.get_speed_mps() == 0);
+	}
+
+	SECTION("no alert when not imminent")
+	{
+		auto_brake.set_collision_threshold_s(2);
+		bus.speed_update_callback(SpeedUpdate{ 100 });
+		bus.car_detected_callback(CarDetected{ 1000, 50 });
+		REQUIRE(bus.commands_published == 0);
+	}
+
+	SECTION("alert when imminent")
+	{
+		auto_brake.set_collision_threshold_s(10);
+		bus.speed_update_callback(SpeedUpdate{ 100 });
+		bus.car_detected_callback(CarDetected{ 100, 0 });
+		REQUIRE(bus.commands_published == 1);
+		REQUIRE(bus.last_command.time_to_collision_s == Catch::Approx(1));
+	}
+
 	
-	bus.speed_update_callback(SpeedUpdate{ 100 });
-	CheckBoolExpression(100 == auto_brake.get_speed_mps(), "speed isn't saved to 100");
-
-	bus.speed_update_callback(SpeedUpdate{ 50 });
-	CheckBoolExpression(50 == auto_brake.get_speed_mps(), "speed isn't saved to 50");
-
-	bus.speed_update_callback(SpeedUpdate{ 0 });
-	CheckBoolExpression(0 == auto_brake.get_speed_mps(), "speed isn't saved to 0");
-}
-
-void AlertWhenImminent()
-{
-	MockServiceBus bus;
-	AutoBrake auto_brake(bus);
-	
-	auto_brake.set_collision_threshold_s(10);
-	bus.speed_update_callback(SpeedUpdate{100});
-	bus.car_detected_callback(CarDetected{ 100, 0 });
-	CheckBoolExpression(bus.commands_published == 1, "brake commands published not one");
-
-	CheckBoolExpression(bus.last_command.time_to_collision_s == 1,
-		"time to collision not computed correctly.");
 }
